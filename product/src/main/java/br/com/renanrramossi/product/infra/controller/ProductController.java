6c83a454d1ac4018e33c866eaec12d3747a579f8
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2023-02-26 21:15:06 -0300

    Add database dml on initialization

--- product/src/main/java/br/com/renanrramossi/product/infra/controller/ProductController.java
+++ product/src/main/java/br/com/renanrramossi/product/infra/controller/ProductController.java
@@ -11,35 +11,56 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-public class ProductController {
+@RequestMapping(path = "/products")
+public class ProductController extends BaseController {
 
   @NonNull
   private final ProductDelegate productDelegate;
 
-  @PostMapping(value = "/products")
-  public ProductDTO create(@NonNull @RequestBody final ProductForm productForm) {
-    return productDelegate.create(productForm);
+  private final PagedResourcesAssembler<ProductDTO> assembler;
+
+  @PostMapping
+  public ResponseEntity<ProductDTO> create(@NonNull @RequestBody final ProductForm productForm) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(productDelegate.create(productForm));
   }
 
-  @GetMapping(value = "/products", produces = {"application/json", "application/xml", "application/x-yaml"})
-  public Page<ProductDTO> findAll(final Pageable pageable) {
-    return productDelegate.findAll(pageable);
+  @GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
+  public ResponseEntity<PagedModel<EntityModel<ProductDTO>>> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+      @RequestParam(value = "limit", defaultValue = "10") int limit,
+      @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+    final Pageable pageable = getPageable(page, limit, direction);
+
+    final Page<ProductDTO> products = productDelegate.findAll(pageable);
+
+    return new ResponseEntity<>(buildPagedModel(assembler, products), HttpStatus.OK);
   }
 
-  public void delete(final Long productId) {
+  @DeleteMapping(value = "/{productId}")
+  public ResponseEntity<Void> delete(@PathVariable("productId") final Long productId) {
       productDelegate.delete(productId);
+      return ResponseEntity.status(HttpStatus.OK).build();
   }
 
-  @GetMapping(value = "/products/{productId}", produces = {"application/json", "application/xml", "application/x-yaml"})
+  @GetMapping(value = "/{productId}", produces = {"application/json", "application/xml", "application/x-yaml"})
   public ProductDTO findById(@PathVariable("productId") final Long productId) {
     final ProductDTO productDTO = productDelegate.findById(productId);
     productDTO.add(linkTo(methodOn(ProductController.class)
@@ -47,7 +68,8 @@ public class ProductController {
     return productDTO;
   }
 
-  public ProductDTO update(final ProductForm productForm) {
+  @PutMapping(value = "/{productId}")
+  public ProductDTO update(@PathVariable("productId") final Long productId, final ProductForm productForm) {
     return productDelegate.update(productForm);
   }
 }
---
+++ product/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into product (id, name, price, stock) values (1, 'product 1', 10.40, 10);
+insert into product (id, name, price, stock) values (2, 'product 2', 20.40, 20);
+insert into product (id, name, price, stock) values (3, 'product 3', 30.40, 30);
+insert into product (id, name, price, stock) values (4, 'product 4', 40.40, 40);
