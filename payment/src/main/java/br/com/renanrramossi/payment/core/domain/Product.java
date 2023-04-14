fa47262d7a8dbd0a88015a43003a9e4c3cf5e23e
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2023-04-14 17:04:32 -0300

    Add Eureka in payment MS

--- payment/src/main/java/br/com/renanrramossi/payment/core/domain/Product.java
+++ payment/src/main/java/br/com/renanrramossi/payment/core/domain/Product.java
@@ -3,6 +3,8 @@ package br.com.renanrramossi.payment.core.domain;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,7 @@ import lombok.NoArgsConstructor;
 public class Product implements Serializable {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @Column(name = "stock", length = 10)
--- payment/src/main/java/br/com/renanrramossi/payment/infra/Application.java
+++ payment/src/main/java/br/com/renanrramossi/payment/infra/Application.java
@@ -3,11 +3,14 @@ package br.com.renanrramossi.payment.infra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EntityScan("br.com.renanrramossi.payment")
 @EnableJpaRepositories("br.com.renanrramossi.payment")
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
